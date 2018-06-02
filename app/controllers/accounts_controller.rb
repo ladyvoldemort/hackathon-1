a022d62bb10b83388b7df2b91a191425707452ab
Michael Tamietti <michael@Michaels-MacBook-Pro.local>
2018-06-02 11:19:35 -0600

    stubbed out controllers

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,22 +1,49 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update]
+  
   def index
+    @accounts = current_user.accounts
   end
 
   def show
   end
 
   def new
+    @account = Account.new
   end
 
   def create
-  end
-
-  def update
+    @account = current_user.accounts.new(account_params)
+    if @account.save
+      redirect_to accounts_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def destroy
+  def update
+    if @account.update(account_params)
+      redirect_to accounts_path
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    @account.destroy
+  end
+
+  private
+
+  def set_account
+    @account = current_user.accounts.find(params[:id])
+  end
+
+  def account_params
+    params.require(:account).permit(:name, :balance)
+  end
+  
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :update, :destroy]
+
   def index
+    @movie = Movie.all
   end
 
   def show
@@ -9,14 +12,38 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      render json: @movie
+    else
+      render_error(@movie)
+    end
   end
 
   def update
+    if @movie.update(movie_params)
+      render json: @movie
+    else
+      render_error(@movie)
+    end
   end
 
   def edit
   end
 
   def destroy
+    @movie.destroy
+    render json: { message: 'removed' }, status: :ok
   end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :duration, :genre, :description, :trailer)
+  end
+  
 end
