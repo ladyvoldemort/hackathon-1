58322ffadc8a815b1cf702aa4b5fad9199825023
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-02 11:18:12 -0600

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,263 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(
+  title: 'The Dark Night',
+  duration: '2hr 32min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0468569/',
+  trailer: 'https://www.youtube.com/embed/EXeTwQWrcwY'
+)
+
+Movie.create(
+  title: 'The Bourne Identity',
+  duration: '1hr 59min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0258463/',
+  trailer: 'https://www.youtube.com/embed/cD-uQreIwEk'
+)
+
+Movie.create(
+  title: 'Avatar',
+  duration: '2hr 42min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0499549/?ref_=fn_al_tt_1',
+  trailer: 'https://www.youtube.com/embed/5PSNL1qE6VY'
+)
+
+Movie.create(
+  title: 'Kill Bill Vol 1',
+  duration: '1hr 51min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0266697/?ref_=nv_sr_1',
+  trailer: 'https://www.youtube.com/embed/ot6C1ZKyiME'
+)
+
+Movie.create(
+  title: 'Gladiator',
+  duration: '2hr 35min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0172495/?ref_=nv_sr_1',
+  trailer: 'https://www.youtube.com/embed/owK1qxDselE'
+)
+
+Movie.create(
+  title: 'The Rock',
+  duration: '2hr 16min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0117500/',
+  trailer: 'https://www.youtube.com/embed/313n0wga2xo'
+)
+
+Movie.create(
+  title: 'Jaws',
+  duration: '2hr 4min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0073195/',
+  trailer: 'https://www.youtube.com/embed/U1fu_sA7XhE'
+)
+
+Movie.create(
+  title: 'Star Wars Episode IV A New Hope',
+  duration: '2hr 1min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0076759/',
+  trailer: 'https://www.youtube.com/embed/1g3_CFmnU7k'
+)
+
+Movie.create(
+  title: 'Star Wars Episode V The Empire Strikes Back',
+  duration: '2hr 4min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0080684/',
+  trailer: 'https://www.youtube.com/embed/mz_YWNhKOkM'
+)
+
+Movie.create(
+  title: 'Star Wars Episode VI Return of the Jedi',
+  duration: '2hr 11min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0086190/',
+  trailer: 'https://www.youtube.com/embed/7L8p7_SLzvU'
+)
+
+Movie.create(
+  title: 'Batman Begins',
+  duration: '2hr 20min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0372784/',
+  trailer: 'https://www.youtube.com/embed/QhPqez3CwiM'
+)
+
+Movie.create(
+  title: 'Top Gun',
+  duration: '1hr 50min',
+  genre: 'Action',
+  description: 'http://www.imdb.com/title/tt0092099/',
+  trailer: 'https://www.youtube.com/embed/qAfbp3YX9F0'
+)
+
+Movie.create(
+  title: 'Dogma',
+  duration: '2hr 10min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0120655/',
+  trailer: 'https://www.youtube.com/embed/20CRw3XdETA'
+)
+
+Movie.create(
+  title: 'Team America World Peace',
+  duration: '1hr 38min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0372588/',
+  trailer: 'https://www.youtube.com/embed/RPBX47zSktc'
+)
+
+Movie.create(
+  title: 'Zoolander',
+  duration: '1hr 30min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0196229/?ref_=nv_sr_2',
+  trailer: 'https://www.youtube.com/embed/YtQq0T3ExLs'
+)
+
+Movie.create(
+  title: 'Superbad',
+  duration: '1hr 53min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0829482/?ref_=nv_sr_1',
+  trailer: 'https://www.youtube.com/embed/MNpoTxeydiY'
+)
+
+Movie.create(
+  title: 'The Hangover',
+  duration: '1hr 40min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt1119646/?ref_=nv_sr_1',
+  trailer: 'https://www.youtube.com/embed/vhFVZsk3XEs'
+)
+
+Movie.create(
+  title: 'Weird Science',
+  duration: '1hr 34min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0090305/',
+  trailer: 'https://www.youtube.com/embed/9qd04u2Yj44'
+)
+
+Movie.create(
+  title: 'Major League',
+  duration: '1hr 47min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0097815/',
+  trailer: 'https://www.youtube.com/embed/7O6QHXQE6Cc'
+)
+
+Movie.create(
+  title: 'The Internship',
+  duration: '1hr 59min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt2234155/',
+  trailer: 'https://www.youtube.com/embed/cdnoqCViqUo'
+)
+
+Movie.create(
+  title: 'Back to the Future',
+  duration: '1hr 56min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0088763/',
+  trailer: 'https://www.youtube.com/embed/qvsgGtivCgs'
+)
+
+Movie.create(
+  title: 'Anchorman',
+  duration: '1hr 34min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0357413/',
+  trailer: 'https://www.youtube.com/embed/NJQ4qEWm9lU'
+)
+
+Movie.create(
+  title: 'Wedding Crashers',
+  duration: '1hr 59min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0396269/',
+  trailer: 'https://www.youtube.com/embed/omI8i1a7rlQ'
+)
+
+Movie.create(
+  title: 'Dazed and Confused',
+  duration: '1hr 42min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0106677/',
+  trailer: 'https://www.youtube.com/embed/3aQuvPlcB-8'
+)
+
+Movie.create(
+  title: 'Ferris Beullers Day Off',
+  duration: '1hr 43min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0091042/',
+  trailer: 'https://www.youtube.com/embed/R-P6p86px6U'
+)
+
+Movie.create(
+  title: "Monty Pythons Holy Grail",
+  duration: '1hr 31min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0071853/',
+  trailer: 'https://www.youtube.com/embed/LG1PlkURjxE'
+)
+
+Movie.create(
+  title: 'Blues Brothers',
+  duration: '2hr 13min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0080455/',
+  trailer: 'https://www.youtube.com/embed/A-xtJYIwfYo'
+)
+
+Movie.create(
+  title: 'Animal House',
+  duration: '1hr 49min',
+  genre: 'Comedy',
+  description: 'http://www.imdb.com/title/tt0077975/',
+  trailer: 'https://www.youtube.com/embed/BoS3-yHoaSY'
+)
+
+Movie.create(
+  title: 'The Godfather',
+  duration: '2hr 55min',
+  genre: 'Drama',
+  description: 'http://www.imdb.com/title/tt0068646/',
+  trailer: 'https://www.youtube.com/embed/5DO-nDW43Ik'
+)
+
+Movie.create(
+  title: 'Rocky',
+  duration: '2hr 00min',
+  genre: 'Drama',
+  description: 'http://www.imdb.com/title/tt0075148/',
+  trailer: 'https://www.youtube.com/embed/3VUblDwa648'
+)
+
+Movie.create(
+  title: 'Forrest Gump',
+  duration: '2hr 22min',
+  genre: 'Drama',
+  description: 'http://www.imdb.com/title/tt0109830/',
+  trailer: 'https://www.youtube.com/embed/YNh9Es8Ut6U'
+)
+
+Movie.create(
+  title: 'Pulp Fiction',
+  duration: '2hr 34min',
+  genre: 'Drama',
+  description: 'http://www.imdb.com/title/tt0110912/',
+  trailer: 'https://www.youtube.com/embed/s7EdQ4FqbhY'
+)
+
+Movie.create(
+  title: 'Swingers',
+  duration: '1hr 36min',
+  genre: 'Drama',
+  description: 'http://www.imdb.com/title/tt0117802/',
+  trailer: 'https://www.youtube.com/embed/nWCct8XbQD0'
+)
