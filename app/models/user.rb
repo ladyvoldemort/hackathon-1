53e800fa25020da96277362178a1a06e26fd6ab2
Brittany V <brittbyu@gmail.com>
2018-06-02 14:07:36 -0600

    oops again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 has_many :comments
 has_many :movies
-has_many :accounts
+has_one :account
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
